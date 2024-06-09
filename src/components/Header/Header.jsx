de306d5b6c31c8b6ad42fbe57d99f9dfcb9cdf35
Dzmitry Yarasheuski <materik08082020@gmail.com>
2024-06-09 17:42:24 +0300

    config eslint, add propTypes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import style from "./Header.module.css";
-import Layout from "../Layout";
-import Logo from "./Logo";
-import Heading from "./Heading";
-import Search from "./Search";
-import Auth from "./Auth";
+import React from 'react';
+import style from './Header.module.css';
+import Layout from '../Layout';
+import Logo from './Logo';
+import Heading from './Heading';
+import Search from './Search';
+import Auth from './Auth';
 
 export const Header = () => {
   return (
