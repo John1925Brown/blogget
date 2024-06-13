6056e538760bff0346f34d29d66845ea618f6e77
Dzmitry Yarasheuski <materik08082020@gmail.com>
2024-06-13 10:29:54 +0300

    divide Post, create, styled Delete, add Delete in Post

--- src/components/Main/List/Post/Post.jsx
+++ src/components/Main/List/Post/Post.jsx
@@ -1,36 +1,22 @@
 import React from 'react';
 import style from './Post.module.css';
-import notphoto from './img/notphoto.jpg';
 import PropTypes from 'prop-types';
-import { formatDate } from '../../../../utils/formatDate';
+
+import Photo from './Photo';
+import Content from './Content';
+import Rating from './Rating';
+import Time from './Time';
+import Delete from './Delete';
 
 export const Post = ({ postData }) => {
   const { title, author, ups, date } = postData;
   return (
     <li className={style.post}>
-      <img className={style.img} src={notphoto} alt={title} />
-
-      <div className={style.content}>
-        <h2 className={style.title}>
-          <a className={style.linkPost} href="#post">
-            {title}
-          </a>
-        </h2>
-
-        <a className={style.linkAuthor} href="#author">
-          {author}
-        </a>
-      </div>
-
-      <div className={style.rating}>
-        <button className={style.up} aria-label="Повысить рейтинг" />
-        <p className={style.ups}>{ups}</p>
-        <button className={style.down} aria-label="Понизить рейтинг" />
-      </div>
-
-      <time className={style.date} dateTime={date}>
-        {formatDate(date)}
-      </time>
+      <Photo title={title} />
+      <Content title={title} author={author} />
+      <Rating ups={ups} />
+      <Time date={date} />
+      <Delete />
     </li>
   );
 };
