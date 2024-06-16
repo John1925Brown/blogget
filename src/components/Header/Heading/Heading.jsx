3bbdd651ef3c8f3019d2b85a459f302849e2ca0e
Dzmitry Yarasheuski <materik08082020@gmail.com>
2024-06-16 08:18:24 +0300

    add comp Text

--- src/components/Header/Heading/Heading.jsx
+++ src/components/Header/Heading/Heading.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import style from './Heading.module.css';
 import PropTypes from 'prop-types';
+import { Text } from '../../../UI/Text/Text';
 
 export const Heading = ({ text }) => {
-  return <div className={style.heading}> {text}</div>;
+  return (
+    <Text As="h1" size={22} tsize={26} center className={style.heading}>
+      {text}
+    </Text>
+  );
 };
 
 Heading.propTypes = {
