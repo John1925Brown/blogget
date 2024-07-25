368ce7dd3e5348e304953137ed22e5e5a6e21a8f
Dzmitry Yarasheuski <materik08082020@gmail.com>
2024-07-25 08:21:41 +0300

    install react devTools

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import { createStore } from 'redux';
 import { getToken, setToken } from '../api/token';
+import { composeWithDevTools } from '@redux-devtools/extension';
 
 const UPDATE_COMMENT = 'UPDATE_COMMENT';
 const UPDATE_TOKEN = 'UPDATE_TOKEN';
@@ -52,4 +53,4 @@ const rootReducer = (state = initialState, action) => {
   }
 };
 
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, composeWithDevTools());
